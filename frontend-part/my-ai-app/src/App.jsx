9514c8dd84183d3b8a603bc6da1921e6a852e59f
mercyogalo <ogalomercy8@gmail.com>
2025-06-11 12:40:24 +0300

    Added tailwind css and configured it

--- frontend-part/my-ai-app/src/App.jsx
+++ frontend-part/my-ai-app/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <div className="flex flex-col bg-red-500 gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+  <img className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/161/746/original/%F0%9F%8D%97Butterflied_Chicken_with_Smokey_Barbecue_Glaze_%F0%9F%8D%97_%281%29.jpeg?1743352387" alt="" />
+  <div className="space-y-2 text-center sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg font-semibold text-black">Erin Lindford</p>
+      <p className="font-medium text-gray-500">Product Engineer</p>
+    </div>
+    <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+      Message
+    </button>
+  </div>
+</div>
+
     </>
   )
 }
